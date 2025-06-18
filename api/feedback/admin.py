d6237350a31ea431003417ac13d979f10df5c472
AlexProv1987 <alex.provenzano87@gmail.com>
2025-06-18 18:44:50 -0500

    feedback admin page

--- api/feedback/admin.py
+++ api/feedback/admin.py
@@ -1,13 +1,37 @@
 from django.contrib import admin
 from .models import *
+from django.core.exceptions import ValidationError
 # Register your models here.
 @admin.register(ApplicantFeedBack)
 class ApplicantFeedBackAdmin(admin.ModelAdmin):
+    list_display=('rating','comment','full_name','created_at')
+    search_fields=('skill_name',)
+    search_help_text='Search By Skill Name'
     
-    list_display=('applicant_reltn__user_reltn__first_name',)
+    fields = ('applicant_reltn',
+              'rating',
+              'comment',
+              'full_name',
+              'email',
+              'company',
+              'ip_address',
+              'user_agent',
+              'created_at',
+              )
+    
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        if request.user.is_superuser:
+           return qs
+        return qs.filter(applicant_reltn__user_reltn=request.user)
+        
+    def get_fields(self, request, obj=None):
+        fields = super().get_fields(request, obj)
+        if request.user.is_superuser:
+            return fields
+        return [f for f in fields if f not in ('applicant_reltn','ip_address','user_agent')]
     
     def get_readonly_fields(self, request, obj=None):
-        return [field.name for field in self.model._meta.concrete_fields]
-    
-    def has_add_permission(self, request):
-            return False
+        if request.user.is_superuser:
+            return ('created_at',)
+        return [f.name for f in self.model._meta.fields]
--- api/feedback/models.py
+++ api/feedback/models.py
@@ -1,15 +1,19 @@
 from django.db import models
 from api.applicant.models import Applicant
 from django.core.validators import MinValueValidator, MaxValueValidator
+from django.utils.translation import gettext_lazy as _
 # Create your models here.
 class ApplicantFeedBack(models.Model):
-    applicant_reltn = models.ForeignKey(Applicant,on_delete=models.CASCADE)
-    rating=models.PositiveIntegerField(validators=[MinValueValidator(1), MaxValueValidator(100)])
-    comment=models.TextField(max_length=2000,null=True,blank=True)
-    full_name = models.CharField(max_length=75)
-    phone_number=models.CharField(max_length=11)
-    email=models.EmailField()
-    company=models.CharField(max_length=125,null=True,blank=True)
-    ip_address=models.GenericIPAddressField()
-    user_agent=models.TextField(blank=True,null=True)
-    created_at=models.DateTimeField(auto_now_add=True)
+    applicant_reltn = models.ForeignKey(Applicant,on_delete=models.CASCADE,verbose_name=_('Applicant'))
+    rating=models.PositiveIntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)],verbose_name=_('Rating'))
+    comment=models.TextField(max_length=2000,null=True,blank=True,verbose_name=_('Comment'))
+    full_name = models.CharField(max_length=75,verbose_name=_('Name'))
+    phone_number=models.CharField(max_length=11,verbose_name=_('Phone'))
+    email=models.EmailField(null=True,blank=True,verbose_name=_('Email'))
+    company=models.CharField(max_length=125,null=True,blank=True,verbose_name=_('Company'))
+    ip_address=models.GenericIPAddressField(verbose_name=_('IP'))
+    user_agent=models.TextField(blank=True,null=True,verbose_name=_('User Agent'))
+    created_at=models.DateTimeField(auto_now_add=True,verbose_name=_('Created'))
+    
+    class Meta:
+        verbose_name_plural=_('Feedback')
